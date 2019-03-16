8239ae69726df4807f9c67e529bef89e63edea4d
Nilav <nilavpatel1992@gmail.com>
2019-03-17 02:45:46 +0530

    add bootstrape plugin, add menu and login screen

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 import './Home.css';
-import { Link } from "react-router-dom";
 import HomeRoutes from "./HomeRoutes"
+import Menus from '../Menu/Menu';
 
 export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home Page</h1>
-                <Link to="/Home/Dashboard"> Dashboard </Link> | 
-                <Link to="/Home/AboutUs"> AboutUs </Link> | 
-                <Link to="/Home/ContactUs"> ContactUs </Link>
-                <HomeRoutes/>
-                <Link to="/Login"> Logout </Link>
+                <Menus/>
+                <HomeRoutes/>                
             </div>
         );
     }
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,13 +1,48 @@
-import React, {  Component } from "react";
+import React, { Component } from "react";
 import './Login.css';
-import { Link } from "react-router-dom";
+import { Form, Button } from 'react-bootstrap';
 
-export default class Login extends Component{
-    render(){
+export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.onLogin = this.onLogin.bind(this);
+        this.state = {
+            isShowErrorMessage: false
+          }
+    }
+
+    onLogin(e) {
+        e.preventDefault();
+        var form = e.currentTarget;
+        if (form.UserName.value === 'admin' && form.Password.value === 'admin123') {
+            this.props.history.push("/Home");
+            return;
+        }
+        this.setState({
+            isShowErrorMessage: true
+          })
+    }
+
+    render() {
         return (
             <div>
-                <h1>Login Page</h1>
-                <Link to="/Home">Home</Link>
+                {this.state.isShowErrorMessage && <div class="alert-text">Invalid credentials</div>}
+                <Form onSubmit={this.onLogin}>
+                    <Form.Group controlId="UserName">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="text" placeholder="Enter user name" />
+                    </Form.Group>
+
+                    <Form.Group controlId="Password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </div>
         );
     }
